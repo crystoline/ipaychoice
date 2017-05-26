9b700d669d7f29ced0c367ca2e4552c1dfaed38c
aisha-alimi <aisha.alimi@aun.edu.ng>
2017-05-26 17:00:16 +0100

    commit

--- app/Http/Controllers/Client/Admin/HomeController.php
+++ app/Http/Controllers/Client/Admin/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Client\Admin;
 use App\Models\Clients\Invoice;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Session;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,9 @@ class HomeController extends Controller
         $data['total_inv'] = Invoice::count();
         $data['total_paid'] = Invoice::whereStatus(1)->count();
         $data['total_pend'] = Invoice::whereStatus(0)->count();
+        $data['officer'] = Session::get('client_admin_officer')->first_name.' '.Session::get('client_admin_officer')->last_name;
 
-        return view('client.admin.dashboard');
+        return view('client.admin.dashboard',['data'=>$data]);
+
     }
 }
--- resources/views/client/admin/dashboard.blade.php
+++ resources/views/client/admin/dashboard.blade.php
@@ -1,119 +1,85 @@
 @extends('client.admin.layouts.master')
 <!--+general-chart("classes", "title", "height", "id", "counter value", "counter desc", tools enabled (use true or false))-->
 @section('stylesheets')
-
+    <link rel="stylesheet" type="text/css" href="{{ asset('client_assets/admin/lib/morrisjs/morris.css')}}"/>
 @endsection
 @php
     $x =40;
 @endphp
 @section('content')
     <div class="row">
-        <div class="col-md-7">
+        <div class="col-md-12">
             <div class="widget widget-pie">
                 <div class="widget-head"><span class="title">Invoices</span></div>
                 <div class="row chart-container">
                     <div class="col-md-6">
-                        <div id="inv_chart" class="chart"></div>
+                        <div id="inv_chart" class="chart"></div><div id="test"></div>
+
                     </div>
                     <div class="col-md-6">
                         <div class="legend"></div>
                     </div>
                 </div>
                 <div class="row chart-info">
-                    <div class="col-xs-4"><span class="title">Total Paid Invoices</span><span data-toggle="counter" data-end="180" class="number">0</span></div>
-                    <div class="col-xs-4"><span class="title">Total Unpaid Invoices</span><span data-toggle="counter" data-end="500" data-prefix="$" class="number">$0</span></div>
-                    <div class="col-xs-4"><span class="title">Total Invoices Created</span><span data-toggle="counter" data-end="90" class="number">0</span></div>
+                    <div class="col-xs-4"><span class="title">Total Paid Invoices</span><span data-toggle="counter" data-end="{{$data['total_paid']}}" class="number">0</span></div>
+                    <div class="col-xs-4"><span class="title">Total Pending Invoices</span><span data-toggle="counter" data-end="{{$data['total_pend']}}" class="number">0</span></div>
+                    <div class="col-xs-4"><span class="title">Total Invoices Created</span><span data-toggle="counter" data-end="{{$data['total_inv']}}" class="number">0</span></div>
                 </div>
             </div>
         </div>
-            <div class="col-md-5">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="widget widget-tile">
-                            <div class="data-info">
-                                <div data-toggle="counter" data-end="18.6" data-decimals="1" data-suffix="%" class="value">0%</div>
-                                <div class="desc">Amount Due</div>
-                            </div>
-                            <div class="icon"><span class="s7-cash"></span></div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="widget widget-tile">
-                            <div class="data-info">
-                                <div data-toggle="counter" data-end="33" data-suffix="%" class="value">0%</div>
-                                <div class="desc">Amount Received</div>
-                            </div>
-                            <div class="icon"><span class="s7-cash"></span></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="widget widget-tile">
-                            <div class="data-info">
-                                <div data-toggle="counter" data-end="156" class="value">0</div>
-                                <div class="desc">Total Customers</div>
-                            </div>
-                            <div class="icon"><span class="s7-users"></span></div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="widget widget-tile">
-                            <div class="data-info">
-                                <div data-toggle="counter" data-decimals="1" data-end="7.5" data-suffix="K" class="value">0</div>
-                                <div class="desc">Total Services</div>
-                            </div>
-                            <div class="icon"><span class="s7-tools"></span></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="widget widget-tile widget-tile-wide">
-                            <div class="tile-info">
-                                <div class="icon"><span class="s7-cash"></span></div>
-                                <div class="data-info">
-                                    <div class="title">Total Invoiced</div>
-                                    <div class="desc">Since Inception</div>
-                                </div>
-                            </div>
-                            <div class="tile-value"><span data-toggle="counter" data-decimals="2" data-end="28458" data-prefix="$">$0</span></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    </div>
+    <br>
+    <a href="admin/new_invoice">
+    <div class="row">
+        <div class="col-md-12">
+
+    <div class="panel panel-full-alt3">
+        <div class="panel-body text-center success"><br>
+            <h2> Hello, {{$data['officer']}}. Create an Invoice Now! </h2><br>
         </div>
+    </div></a>
+        </div>
+    </div>
 @endsection
 
 @section('javascript')
-
-    <script type="text/javascript">
-        $(document).ready(function(){
-            //initialize the javascript
-            App.init();
-        App.dashboard();
-        });
-    </script>
-
     <script src="{{ asset('client_assets/admin/lib/jquery-flot/jquery.flot.js')}}" type="text/javascript"></script>
     <script src="{{ asset('client_assets/admin/lib/jquery-flot/jquery.flot.pie.js')}}" type="text/javascript"></script>
     <script src="{{ asset('client_assets/admin/lib/jquery-flot/jquery.flot.resize.js')}}" type="text/javascript"></script>
     <script src="{{ asset('client_assets/admin/lib/countup/countUp.min.js')}}" type="text/javascript"></script>
     <script src="{{ asset('client_assets/admin/lib/chartjs/Chart.min.js')}}" type="text/javascript"></script>
+    <script src="{{ asset('client_assets/admin/lib/raphael/raphael-min.js')}}" type="text/javascript"></script>
+    <script src="{{ asset('client_assets/admin/lib/morrisjs/morris.min.js')}}" type="text/javascript"></script>
 
-    @php
-    echo '<script type="text/javascript">
-        function b(){
-            var a=[
-                    {label:"Paid Invoices",data:0},
-                    {label:"Unpaid Invoices",data:'.$x.'}],
-                b=tinycolor("#7accbe").lighten(5).toString(),
-                c="#ef6262",
-                d=App.color.alt1,
-                e=$("#inv_chart").parent().next().find(".legend");
-            $.plot("#inv_chart",a,{series:{pie:{show:!0,highlight:{opacity:.1}}},grid:{hoverable:!0},legend:{container:e},colors:[b,c,d]})
-        }
-        b()
-    </script>';
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            //initialize the javascript
+            App.init();
+
+            App.dashboard();
+        });
+    </script>
+
+     @php
+     if ($data['total_paid'] > 0) {
+        echo '<script type="text/javascript">
+            function b(){
+                var a=[
+                        {label:"Paid Invoices",data:'.$data['total_paid'].'},
+                        {label:"Unpaid Invoices",data:'.$data['total_pend'].'}],
+                    b=tinycolor("#7accbe").lighten(5).toString(),
+                    c="#ef6262",
+                    d=App.color.alt1,
+                    e=$("#inv_chart").parent().next().find(".legend");
+                $.plot("#inv_chart",a,{series:{pie:{show:!0,highlight:{opacity:.1}}},grid:{hoverable:!0},legend:{container:e},colors:[b,c,d]})
+            }
+            b()
+        </script>';
+     } else{
+     echo '<script type="text/javascript">
+     document.getElementById("#test").innerHTML = "You have not created any invoices yet!"
+     </script>';
+     }
     @endphp
 @endsection
